05b47d856e6d3673331d695f81662940badfd8a8
raj kadam <kadamraj623@gmail.com>
2024-07-24 20:58:12 +0530

    fix (dev): add footer

--- property-rental-app/my-app/src/app/pages/componets/home/home.component.ts
+++ property-rental-app/my-app/src/app/pages/componets/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from "../../../shared/reuseable/search/search.component";
+import { CardComponent } from "../../../shared/reuseable/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
