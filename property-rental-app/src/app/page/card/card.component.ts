12169a5a2ac37d126e5551eb1480d6b1a7374282
raj kadam <kadamraj623@gmail.com>
2024-08-01 20:21:45 +0530

    dev (fix): add cards dynamiclly

--- property-rental-app/src/app/page/card/card.component.ts
+++ property-rental-app/src/app/page/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { api } from 'src/app/core/constant/api/api';
+import { CacheJsonDataService } from 'src/app/core/service/cache-json-data.service';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  cardData: any[] = [];
 
+  constructor(public faq: CacheJsonDataService) { }
+
+  ngOnInit(): void {
+    this.faq.parseJsonFile(api.cards).subscribe((res: any) => {
+      this.cardData = res;
+    });
+  }
 }
