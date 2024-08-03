d5de975e89c3e01247e6ac5375905ab653d8adf3
raj kadam <kadamraj623@gmail.com>
2024-08-03 22:05:18 +0530

    dev (fix): add forms and validations

--- property-rental-app/src/app/page/forms/request-tourform/request-tourform.component.ts
+++ property-rental-app/src/app/page/forms/request-tourform/request-tourform.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-request-tourform',
   templateUrl: './request-tourform.component.html',
-  styleUrls: ['./request-tourform.component.css']
+  styleUrls: ['./request-tourform.component.css'],
 })
 export class RequestTourformComponent {
+  valid: boolean = false;
 
+  constructor() { }
+  requestTourForm: FormGroup = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+    ]),
+    tourDate: new FormControl('', [Validators.required]),
+    acceptTerms: new FormControl(false, [Validators.requiredTrue]),
+  });
+
+  onSubmit() {
+    this.valid = true;
+    if (this.requestTourForm.invalid) {
+      return;
+    }
+    console.log(this.requestTourForm.value);
+  }
+
+  reset() {
+    this.requestTourForm.reset();
+  }
 }
