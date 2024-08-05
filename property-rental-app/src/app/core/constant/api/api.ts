995a23ac0fbd0f0a5cc3e60325310f4400f26fe3
raj kadam <kadamraj623@gmail.com>
2024-08-05 19:35:14 +0530

    dev (fix): change folder name

--- property-rental-app/src/app/core/constant/api/api.ts
+++
@@ -1,7 +0,0 @@
-export const api = {
-    footer:"assets/master/footer.json",
-    featurette:"assets/master/featurette.json",
-    faqs:"assets/master/faqs.json",
-    cards:"assets/master/card.json",
-    propertyData:"assets/master/propertydata.json"
-}
