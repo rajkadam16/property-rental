36cadd6f6f10985287a5511728617ccdd20bb849
raj kadam <kadamraj623@gmail.com>
2024-12-28 21:01:24 +0530

    fix(dev):working on floor plan component

--- property-rental-main-app/src/app/page/property-media-viewer/property-media-viewer.component.ts
+++ property-rental-main-app/src/app/page/property-media-viewer/property-media-viewer.component.ts
@@ -1,10 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PropertyFloorPlanComponent } from 'src/app/components/property-floor-plan/property-floor-plan.component';
-import { PropertyPhotosComponent } from 'src/app/components/property-photos/property-photos.component';
+import { Location } from '@angular/common';
 
-import { TabModel } from 'src/app/core/models/interface';
-import { CommonUtilitiesService } from 'src/app/core/service/common-utilities.service';
 
 @Component({
   selector: 'app-property-media-viewer',
@@ -15,38 +11,11 @@ export class PropertyMediaViewerComponent {
 
   @Input("propertyName") propertyName:any;
   @Input("media") mediaData:any;
-  // @Input("propertyCard") propertyCard:any;
-  
-  tabDataList: TabModel[] = [{
-    id: "photo",
-    displayName: "Photos",
-    defaultActive: "active",
-    selected: true,
-    components: [
-      PropertyPhotosComponent
-    ]
-  },
-  {
-    id: "floor",
-    displayName: "Floor",
-    defaultActive: "",
-    selected: false,
-    components: [
-      PropertyFloorPlanComponent
-    ]
+
+
+constructor(private readonly location: Location){}
+goBack() {
+  this.location.back(); 
   }
-  ];
-
-  onTabHover(event: Event) {
-    const button = event.target as HTMLButtonElement;
-    button.style.backgroundColor = '#495057'; // Lighter shade
-    button.style.transform = 'scale(1.05)'; // Slight zoom effect
-}
-
-onTabLeave(event: Event) {
-    const button = event.target as HTMLButtonElement;
-    button.style.backgroundColor = ''; // Reverts to default
-    button.style.transform = 'scale(1)'; // Reset scale
-}
 }
      
