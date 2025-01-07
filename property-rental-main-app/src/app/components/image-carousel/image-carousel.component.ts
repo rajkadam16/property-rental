be4df9e326c1f09571a747700dd9851757f4abc9
Ramdas Kadam <ramdaskadam90@gmail.com>
2025-01-06 22:18:25 -0600

    fix(rk): image componet gallery issue

--- property-rental-main-app/src/app/components/image-carousel/image-carousel.component.ts
+++ property-rental-main-app/src/app/components/image-carousel/image-carousel.component.ts
@@ -1,10 +1,47 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PropertyImages } from 'src/app/core/models/interface';
 
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.css']
 })
-export class ImageCarouselComponent {
-  @Input("data") imageCarousel: any; 
+export class ImageCarouselComponent implements OnInit {
+  @Input("data") imageCarousel: PropertyImages | undefined;
+
+  currentIndex: number = 0;
+  interval: any;
+
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  startCarousel(): void {
+    this.interval = setInterval(() => {
+      this.nextImage();
+    }, 3000); // Change image every 3 seconds
+  }
+
+  stopCarousel(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+
+  nextImage(): void {
+    if (this.imageCarousel?.images) {
+      this.currentIndex = (this.currentIndex + 1) % this.imageCarousel.images.length;
+    }
+  }
+
+  previousImage(): void {
+    if (this.imageCarousel?.images) {
+      this.currentIndex = (this.currentIndex - 1 + this.imageCarousel.images.length) % this.imageCarousel.images.length;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.stopCarousel();
+  }
 }
--- property-rental-main-app/src/app/core/models/interface.ts
+++ property-rental-main-app/src/app/core/models/interface.ts
@@ -6,3 +6,14 @@ export interface TabModel {
     selected: boolean
     components : any[]
   }
+
+  // property-photos.model.ts
+export interface Image {
+  url: string;
+  alt: string;
+}
+
+export interface PropertyImages {
+  id: string;
+  images: Image[];
+}
