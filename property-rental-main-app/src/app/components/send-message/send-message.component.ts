f372f8121f7cb5d7632d8fb426406909a0d7701c
raj kadam <kadamraj623@gmail.com>
2025-01-11 15:42:55 +0530

    fix(rjk):fix from email issue

--- property-rental-main-app/src/app/components/send-message/send-message.component.ts
+++ property-rental-main-app/src/app/components/send-message/send-message.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-send-message',
@@ -9,9 +10,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SendMessageComponent {
   @Input("propertyInfo") propertyInfo:any
     submitted: boolean = false;
+   
 
   constructor() { }
-  requestTourForm: FormGroup = new FormGroup({
+  sendMessage: FormGroup = new FormGroup({
     firstName: new FormControl('', [
       Validators.required,
       Validators.maxLength(20),
@@ -27,19 +29,37 @@ export class SendMessageComponent {
     ]),
     tourDate: new FormControl('', [Validators.required]),
     acceptTerms: new FormControl(false, [Validators.requiredTrue]),
+    coustomMessage: new FormControl('', []),
   });
 
   onSubmit() {
     this.submitted = true;
-    if (this.requestTourForm.invalid) {
+    if (this.sendMessage.invalid) {
       return;
     }
-    // console.log(JSON.stringify(this.requestTourForm.value));
+    console.log(JSON.stringify(this.sendMessage.value));
   }
 
-  reset() {
-    this.submitted = false;
-    this.requestTourForm.reset();
-  }
 
+
+  async sendEmail() {
+    emailjs.init('okGSde90IJRSKKcdy');
+    let response = await emailjs.send("service_r7mi3h9","template_bj18bsj",{
+        from_name: this.sendMessage.value.firstName + " " + this.sendMessage.value.lastName,
+        firstname: this.sendMessage.value.firstName,
+        propertyname: this.propertyInfo.propertyName,
+        date: this.sendMessage.value.tourDate,
+        phoneNo: this.sendMessage.value.phoneNumber,
+        message: this.sendMessage.value.coustomMessage,
+        reply_to: this.sendMessage.value.email,
+    });
+    console.log('sending request for tour', response.status);
+    alert('Mesaage sent successfully');
+    
+}
+
+reset() {
+  this.submitted = false;
+  this.sendMessage.reset();
+}
 }
