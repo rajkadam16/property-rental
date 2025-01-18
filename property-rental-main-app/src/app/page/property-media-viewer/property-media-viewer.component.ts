42702731cbb99eb01cd24588f5f726035c87e5e1
raj kadam <kadamraj623@gmail.com>
2025-01-19 00:48:44 +0530

    fix(rjk):update font

--- property-rental-main-app/src/app/page/property-media-viewer/property-media-viewer.component.ts
+++ property-rental-main-app/src/app/page/property-media-viewer/property-media-viewer.component.ts
@@ -16,8 +16,8 @@ export class PropertyMediaViewerComponent {
   propertyName: any[] = [];
   propertyFloorPlans: PropertyImages | undefined;
   propertyPhotos: PropertyImages | undefined;
-  propertyMapPositionInLong: number = 0;
-  propertyMapPositionInLat: number = 0;
+  propertyMapPositionInLong: number = 0; //latitude 
+  propertyMapPositionInLat: number = 0; //longitude
 
   propertyImageCarouselStyle: any = {
     'object-fit': 'cover', // Ensures images are cropped to fill the container
@@ -28,14 +28,10 @@ export class PropertyMediaViewerComponent {
     'max-width': '600px',  // Adjust as needed for your carousel width
   };
 
-
-
-
   constructor(private readonly location: Location,
     private readonly route: ActivatedRoute,
     private readonly propertyDataService: CommonUtilitiesService
   ) { }
-
 
   ngOnInit(): void {
     const productId = Number(this.route.parent?.snapshot.paramMap.get('id'));
@@ -44,8 +40,8 @@ export class PropertyMediaViewerComponent {
       this.propertyName = response.propertyName;
       this.propertyFloorPlans = response.propertyFloorPlans;
       this.propertyPhotos = response.propertyPhotos;
-      this.propertyMapPositionInLat = parseFloat(response.propertyMapPositionInLat);
-      this.propertyMapPositionInLong = parseFloat(response.propertyMapPositionInLong)
+      this.propertyMapPositionInLat = parseFloat(response.propertyMapPositionInLat); //getting the latitude
+      this.propertyMapPositionInLong = parseFloat(response.propertyMapPositionInLong) //getting the longitude
     });
   }
 
