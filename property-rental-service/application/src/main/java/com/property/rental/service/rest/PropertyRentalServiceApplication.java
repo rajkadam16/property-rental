3e942e44af863302938a80c67e8b7192ab86cecd
Ramdas Kadam <ramdaskadam90@gmail.com>
2025-01-26 16:19:40 -0600

    fix(rk): integrated db

--- property-rental-service/application/src/main/java/com/property/rental/service/rest/PropertyRentalServiceApplication.java
+++ property-rental-service/application/src/main/java/com/property/rental/service/rest/PropertyRentalServiceApplication.java
@@ -3,9 +3,11 @@ package com.property.rental.service.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.property.rental.service.*")
+@EnableMongoRepositories(basePackages = "com.property.rental.service.core.api.db")
 public class PropertyRentalServiceApplication {
 
 	public static void main(String[] args) {
