1d9b217f68bd0e8a23cbd743eeaaaf73775c2d7b
raj kadam <kadamraj623@gmail.com>
2025-02-20 21:28:25 +0530

    fix(rjk):login and signup done

--- property-rental-main-app/src/app/core/constant/ApiConfig.ts
+++ property-rental-main-app/src/app/core/constant/ApiConfig.ts
@@ -2,10 +2,10 @@ export const ApiConfig = {
     footer: "assets/master/footer.json",
     featurette: "assets/master/featurette.json",
     faqs: "assets/master/faqs.json",
-    // propertyData: "http://localhost:8080/property/get/all", //main backend api 
-    propertyData:"assets/master/propertydata.json", //json file
+    propertyData: "http://localhost:8080/property/get/all", //main backend api 
+    // propertyData:"assets/master/propertydata.json", //json file
     addProperty: "http://localhost:8080/property/create",//backend api
-    navbar:"assets/master/navbar.json",  //json file
-    // navbar: "http://localhost:8080/navbar/get/all", //backend api
+    // navbar:"assets/master/navbar.json",  //json file
+    navbar: "http://localhost:8080/navbar/get/all", //backend api
     pageNotFound: "assets/master/pagenotfound.json",
 }
